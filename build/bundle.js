c8073290b4634be2c40f0363c65be7ef244567d2
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-19 00:39:41 +0100

    Folder and files added for cleaner code

--- build/bundle.js
+++ build/bundle.js
@@ -1,91 +1,4 @@
 "use strict";
-class Star {
-    constructor() {
-        this.x = random(width);
-        this.y = random(-height, 0);
-        this.speed = random(1, 5);
-    }
-    show() {
-        stroke(255);
-        point(this.x, this.y);
-    }
-    fall() {
-        this.y += this.speed;
-        if (this.y > height) {
-            this.y = random(-height, 0);
-        }
-    }
-}
-class Atmosphere {
-    constructor() {
-        this.y = 0;
-        this.speed = 2;
-        this.alphaValue = 255;
-        this.isDisplayed = false;
-    }
-    display() {
-        noStroke();
-        fill(135, 206, 235, this.alphaValue);
-        ellipse(width / 2, this.y + height / 2, width * 2.3, height * 2);
-        this.isDisplayed = true;
-    }
-    move() {
-        this.y += this.speed;
-        this.alphaValue -= .4;
-        if (!this.isDisplayed) {
-            this.y += this.speed;
-            this.alphaValue -= .5;
-        }
-    }
-}
-class Earth {
-    constructor() {
-        this.y = 0;
-        this.speed = 2;
-        this.isDisplayed = false;
-    }
-    display() {
-        noStroke();
-        fill(34, 139, 34);
-        ellipse(width / 2, this.y + height / 1.2, width * 2.3, height * 0.8);
-        this.isDisplayed = true;
-    }
-    move() {
-        this.y += this.speed;
-        if (!this.isDisplayed) {
-            this.y += this.speed;
-        }
-    }
-}
-class Background {
-    constructor() {
-        this.stars = [];
-        for (let i = 0; i < 1000; i++) {
-            this.stars[i] = new Star();
-        }
-        this.atmosphere = new Atmosphere();
-        this.earth = new Earth();
-    }
-    update() {
-        for (let i = 0; i < this.stars.length; i++) {
-            this.stars[i].fall();
-        }
-        this.atmosphere.move();
-        this.atmosphere.display();
-        this.earth.move();
-        this.earth.display();
-    }
-    draw() {
-        background(0);
-        for (let i = 0; i < this.stars.length; i++) {
-            this.stars[i].show();
-        }
-        this.atmosphere.move();
-        this.atmosphere.display();
-        this.earth.move();
-        this.earth.display();
-    }
-}
 class Game {
     constructor() {
         this.gameEngine = new GameEngine();
@@ -142,6 +55,93 @@ function draw() {
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+class Atmosphere {
+    constructor() {
+        this.y = 0;
+        this.speed = 2;
+        this.alphaValue = 255;
+        this.isDisplayed = false;
+    }
+    display() {
+        noStroke();
+        fill(135, 206, 235, this.alphaValue);
+        ellipse(width / 2, this.y + height / 2, width * 2.3, height * 2);
+        this.isDisplayed = true;
+    }
+    move() {
+        this.y += this.speed;
+        this.alphaValue -= .4;
+        if (!this.isDisplayed) {
+            this.y += this.speed;
+            this.alphaValue -= .5;
+        }
+    }
+}
+class Earth {
+    constructor() {
+        this.y = 0;
+        this.speed = 2;
+        this.isDisplayed = false;
+    }
+    display() {
+        noStroke();
+        fill(34, 139, 34);
+        ellipse(width / 2, this.y + height / 1.2, width * 2.3, height * 0.8);
+        this.isDisplayed = true;
+    }
+    move() {
+        this.y += this.speed;
+        if (!this.isDisplayed) {
+            this.y += this.speed;
+        }
+    }
+}
+class Star {
+    constructor() {
+        this.x = random(width);
+        this.y = random(-height, 0);
+        this.speed = random(1, 5);
+    }
+    show() {
+        stroke(255);
+        point(this.x, this.y);
+    }
+    fall() {
+        this.y += this.speed;
+        if (this.y > height) {
+            this.y = random(-height, 0);
+        }
+    }
+}
+class Background {
+    constructor() {
+        this.stars = [];
+        for (let i = 0; i < 1000; i++) {
+            this.stars[i] = new Star();
+        }
+        this.atmosphere = new Atmosphere();
+        this.earth = new Earth();
+    }
+    update() {
+        for (let i = 0; i < this.stars.length; i++) {
+            this.stars[i].fall();
+        }
+        this.atmosphere.move();
+        this.atmosphere.display();
+        this.earth.move();
+        this.earth.display();
+    }
+    draw() {
+        background(0);
+        for (let i = 0; i < this.stars.length; i++) {
+            this.stars[i].show();
+        }
+        this.atmosphere.move();
+        this.atmosphere.display();
+        this.earth.move();
+        this.earth.display();
+    }
 }
 class Alien extends Enemy {
     constructor() {
