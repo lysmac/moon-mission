d731bfe04badbe9933b17173162721cc54e833be
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-18 19:55:56 +0100

    first draft earth and atmosphere

--- build/bundle.js
+++ build/bundle.js
@@ -16,23 +16,74 @@ class Star {
         }
     }
 }
+class Atmosphere {
+    constructor() {
+        this.y = 0;
+        this.speed = 2;
+        this.alphaValue = 255;
+        this.isDisplayed = false;
+    }
+    display() {
+        noStroke();
+        fill(135, 206, 235, this.alphaValue);
+        ellipse(width / 2, this.y + height / 2, width * 2.3, height * 2);
+        this.isDisplayed = true;
+    }
+    move() {
+        this.y += this.speed;
+        this.alphaValue -= .4;
+        if (!this.isDisplayed) {
+            this.y += this.speed;
+            this.alphaValue -= .5;
+        }
+    }
+}
+class Earth {
+    constructor() {
+        this.y = 0;
+        this.speed = 2;
+        this.isDisplayed = false;
+    }
+    display() {
+        noStroke();
+        fill(34, 139, 34);
+        ellipse(width / 2, this.y + height / 1.2, width * 2.3, height * 0.8);
+        this.isDisplayed = true;
+    }
+    move() {
+        this.y += this.speed;
+        if (!this.isDisplayed) {
+            this.y += this.speed;
+        }
+    }
+}
 class Background {
     constructor() {
         this.stars = [];
         for (let i = 0; i < 1000; i++) {
             this.stars[i] = new Star();
         }
+        this.atmosphere = new Atmosphere();
+        this.earth = new Earth();
     }
     update() {
         for (let i = 0; i < this.stars.length; i++) {
             this.stars[i].fall();
         }
+        this.atmosphere.move();
+        this.atmosphere.display();
+        this.earth.move();
+        this.earth.display();
     }
     draw() {
         background(0);
         for (let i = 0; i < this.stars.length; i++) {
             this.stars[i].show();
         }
+        this.atmosphere.move();
+        this.atmosphere.display();
+        this.earth.move();
+        this.earth.display();
     }
 }
 class Game {
