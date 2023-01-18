f2123b28f4d00cb84b3537cb56482e62f3585bfb
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-01-18 11:11:08 +0100

    structure gameClass for gameMenu

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,30 @@
+// Is this right or should it be in a separate file?
+interface IstartGame {
+  startNewGame(): void
+  resumeGame(): void
+}
 class Game {
-  constructor() {}
+  private gameMenu: GameMenu
 
-  public update() {}
+  // playerScore är nu kopplad till GameEntity i klassschemat, kan behöva flyttas/ändras
+  private playerScore: Score
+  private gameEngine: GameEngine
+  private currentScene: string
 
-  public draw() {}
+  constructor() {
+    this.gameMenu = new GameMenu()
+
+    // new GameMenu(this)
+    // Stod i klassschemat, vet inte exakt hur den ska användas?
+    
+
+  }
+
+  public update(): void {}
+
+  public draw(): void {}
+
+  public startNewGame(): void {}
+
+  public resumeGame(): void {}
 }
