13b09f18a59167ed9e725eb97e2644321f77905b
lysmac <calle@lysmask.se>
2023-01-19 11:51:05 +0100

    can pause but not resume (yet)

--- src/game.ts
+++ src/game.ts
@@ -4,39 +4,35 @@
 //   resumeGame(): void
 // }
 class Game {
-
   private gameEngine: GameEngine;
-  private gameMenu: GameMenu
-   private playerScore: Score
- 
-  private currentScene: string
-  
+  private gameMenu: GameMenu;
+  private pauseMenu: PauseMenu;
+  private playerScore: Score;
+
+  private currentScene: string;
+
   constructor() {
-    this.gameMenu = new GameMenu(100, 300, 400, 300, "#566E93")
-    this.gameEngine = new GameEngine()
+    this.gameMenu = new GameMenu(100, 300, 400, 300, "#566E93");
+    this.pauseMenu = new PauseMenu(100, 300, 400, 300, "#566E93");
+    this.gameEngine = new GameEngine();
   }
-    // new GameMenu(this)
-    // Stod i klassschemat, vet inte exakt hur den ska användas?
+  // new GameMenu(this)
+  // Stod i klassschemat, vet inte exakt hur den ska användas?
 
   public update(): void {
     this.gameEngine.update();
-    this.gameMenu.update()
+    this.gameMenu.update();
+    this.pauseMenu.update();
   }
 
   public draw(): void {
-    this.gameMenu.draw()
-    // this.gameEngine.draw();
+    this.gameMenu.draw();
+    this.pauseMenu.draw();
+
+    this.gameEngine.draw();
   }
 
-
-
-  
-
- 
-  
-    public startNewGame(): void {}
+  public startNewGame(): void {}
 
   public resumeGame(): void {}
-
 }
-
