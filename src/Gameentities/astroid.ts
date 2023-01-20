1a4a7088d2bb9d209c7f7ba97f307fb6fb1a02b1
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-20 15:14:05 +0100

    Code correction

--- src/Gameentities/astroid.ts
+++ src/Gameentities/astroid.ts
@@ -1,11 +1,16 @@
 /// <reference path="enemy.ts" />
 
 class Astroid extends Enemy {
-  constructor() {
-    super();
+  // private image: p5.Image;
+  // private speed: p5.Vector;
+  // private size: p5.Vector;
+
+  constructor(position: p5.Vector) {
+    const size = createVector(random(100, 500), random(100, 500));
+    super(position, size, asteroid);
+
+    // this.image = loadImage('/assets/Astroid.png');
+    // this.speed = createVector(0, random(2, 10));
+    // this.size = createVector(random(100, 500), random(100, 500));
   }
-
-  public update() {}
-
-  public draw() {}
 }
