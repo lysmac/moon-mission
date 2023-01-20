ccfd794853afc5dc6200ce580bcbcc7f4d9c165f
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-20 15:12:47 +0100

    Starfall background added to gameMenu

--- src/gamemenu.ts
+++ src/gamemenu.ts
@@ -10,6 +10,7 @@ class GameMenu {
   textHowToPlay: string = "HOW TO PLAY";
   textColor: string = "black";
   game: IStartGame;
+  background: Background; 
 
   constructor(
     game: IStartGame,
@@ -22,34 +23,38 @@ class GameMenu {
     this.game = game;
     this.x = x;
     this.y = y;
+    // this.position = createVector(0, 0)
     this.width = width;
     this.height = height;
     this.color = color;
+    this.background = new Background(true);
   }
 
   public update() {
     if (true) {
       this.game.startNewGame();
     }
+
+    this.background.update();
   }
-
+  
   public draw() {
+    this.background.draw();
     //background(255, 204, 0);
-
+    
     fill(this.color);
+    // rectMode(CENTER);
     rect(this.x, this.y, this.width, this.height, 20);
     noStroke();
-
+    
     textSize(this.textSize);
-    textAlign(CENTER - textWidth(this.text));
+    textAlign(CENTER, CENTER);
     fill(this.textColor);
     textFont("times new roman");
     text(this.text, this.x + 140, this.y + 30);
-    textAlign(CENTER - textWidth(this.textPlay));
     text(this.textPlay, this.x + 160, this.y + 60);
-    textAlign(CENTER - textWidth(this.textHowToPlay));
     text(this.textHowToPlay, this.x + 130, this.y + 90);
-
+    
     image(img, 0, 0);
   }
 }
