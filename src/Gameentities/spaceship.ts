6c142afb0e6291e4e36e061f8c8498b2115802b4
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-22 17:45:05 +0100

    base logic for spaceship

--- src/Gameentities/spaceship.ts
+++ src/Gameentities/spaceship.ts
@@ -1,9 +1,21 @@
 class SpaceShip extends GameEntity {
-    constructor() {
-        super();
+    constructor(position: p5.Vector) {
+        const size = createVector(100, 400);
+        super(position, size, raket3);
+    }
+
+    private moveSpaceship() {
+        if (keyIsDown(LEFT_ARROW) && this.position.x > 0) {
+            this.position.x -= 5;
+        }
+        if (keyIsDown(RIGHT_ARROW) && this.position.x < width - this.size.x) {
+            this.position.x += 5;
+        }
+      }
+  
+    public update() {
+        this.moveSpaceship();
     }
   
-    public update() {}
-  
-    public draw() {}
+    // public draw() {}
   }
