d0fb88239d25f7976f781daed38ab95bb9690697
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-22 20:32:23 +0100

    Logic for movement of spaceship now correct

--- src/Gameentities/spaceship.ts
+++ src/Gameentities/spaceship.ts
@@ -1,21 +1,21 @@
 class SpaceShip extends GameEntity {
-    constructor(position: p5.Vector) {
-        const size = createVector(100, 400);
-        super(position, size, raket3);
-    }
-
-    private moveSpaceship() {
-        if (keyIsDown(LEFT_ARROW) && this.position.x > 0) {
-            this.position.x -= 5;
-        }
-        if (keyIsDown(RIGHT_ARROW) && this.position.x < width - this.size.x) {
-            this.position.x += 5;
-        }
-      }
-  
-    public update() {
-        this.moveSpaceship();
-    }
-  
-    // public draw() {}
+  constructor(position: p5.Vector) {
+    const size = createVector(100, 400);
+    super(position, size, raket3);
   }
+
+  private moveSpaceship() {
+    if (keyIsDown(LEFT_ARROW) && this.position.x > 0) {
+      this.position.x -= 10;
+    }
+    if (keyIsDown(RIGHT_ARROW) && this.position.x < width - this.size.x) {
+      this.position.x += 10;
+    }
+  }
+
+  public update() {
+    this.moveSpaceship();
+  }
+
+  // public draw() {}
+}
