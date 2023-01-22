b94142b3d35c7cd60fe49ca2782e0ac52f21755e
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-22 22:41:11 +0100

    completed spaceship logic for moving + animation

--- src/Gameentities/spaceship.ts
+++ src/Gameentities/spaceship.ts
@@ -1,7 +1,16 @@
 class SpaceShip extends GameEntity {
+  private images: p5.Image[];
+  private currentImageIndex: number;
+  private timer: number;
+  private delay: number;
+
   constructor(position: p5.Vector) {
-    const size = createVector(100, 400);
+    const size = createVector(50, 200);
     super(position, size, raket3);
+    this.images = [raket3, raket4, raket5];
+    this.currentImageIndex = 0;
+    this.timer = 0;
+    this.delay = 20;
   }
 
   private moveSpaceship() {
@@ -15,7 +24,13 @@ class SpaceShip extends GameEntity {
 
   public update() {
     this.moveSpaceship();
+    this.timer++;
+    if (this.timer % this.delay === 0) {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }
   }
 
-  // public draw() {}
+  public draw() {
+    image(this.images[this.currentImageIndex], this.position.x, this.position.y, this.size.x, this.size.y);
+  }
 }
