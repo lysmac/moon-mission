6c42bd3f58b59c47fecd6eb198a9f49816dcb6cb
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-01-23 08:56:57 +0100

    minor fix

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,22 @@
 // Is this right or should it be in a separate file?
 interface IStartGame {
-  startNewGame(): void;
-  resumeGame(): void;
+  startNewGame(): void
+  resumeGame(): void
 }
 
 class Game implements IStartGame {
-  private gameEngine: GameEngine;
-  private gameMenu: GameMenu;
-  private pauseMenu: PauseMenu;
+  private gameEngine: GameEngine
+  private gameMenu: GameMenu
+  private pauseMenu: PauseMenu
   // private playerScore: Score;
 
-  private currentScene: "start" | "play" | "pause" | "end";
+  private currentScene: "start" | "play" | "pause" | "end"
 
   constructor() {
-    this.gameMenu = new GameMenu(this, 100, 300, 400, 300, "#566E93");
-    this.pauseMenu = new PauseMenu(100, 300, 400, 300, "#566E93");
-    this.gameEngine = new GameEngine();
-    this.currentScene = "start";
+    this.gameMenu = new GameMenu(this, 100, 300, 400, 300, "#566E93")
+    this.pauseMenu = new PauseMenu(100, 300, 400, 300, "#566E93")
+    this.gameEngine = new GameEngine()
+    this.currentScene = "start"
   }
   // new GameMenu(this)
   // Stod i klassschemat, vet inte exakt hur den ska användas?
@@ -24,28 +24,28 @@ class Game implements IStartGame {
   public update(): void {
     switch (this.currentScene) {
       case "start":
-        this.gameMenu.update();
-        break;
+        this.gameMenu.update()
+        break
       case "play":
-        this.gameEngine.update();
-        break;
+        this.gameEngine.update()
+        break
       case "pause":
-        this.pauseMenu.update();
-        break;
+        this.pauseMenu.update()
+        break
     }
   }
 
   public draw(): void {
     switch (this.currentScene) {
       case "start":
-        this.gameMenu.draw();
-        break;
+        this.gameMenu.draw()
+        break
       case "play":
-        this.gameEngine.draw();
-        break;
+        this.gameEngine.draw()
+        break
       case "pause":
-        this.pauseMenu.draw();
-        break;
+        this.pauseMenu.draw()
+        break
     }
   }
 
