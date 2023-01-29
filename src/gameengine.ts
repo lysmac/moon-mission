c3d1b53ff497cad793ca122a8d87e6160e89a7af
ye-mou <git.ymo22@gmail.com>
2023-01-29 17:36:51 +0100

    killing enemy now functioning

--- src/gameengine.ts
+++ src/gameengine.ts
@@ -4,7 +4,9 @@ class GameEngine {
   private background: Background;
   private gameEntities: GameEntity[];
   private clonedGameEntitiy: GameEntity[] = [];
-  private spawnTimout: number;
+  private asteroidSpawnTimout: number;
+  private alienSpawnTimout: number;
+  private oxygenSpawnTimout: number;
   private score: number;
   private isScoreBlinking: boolean;
   private dead: boolean;
@@ -18,7 +20,9 @@ class GameEngine {
     this.oxygenDisplay = new OxygenDisplay();
     this.spaceship = new SpaceShip();
     this.gameEntities = [];
-    this.spawnTimout = 2000;
+    this.asteroidSpawnTimout = 1000;
+    this.alienSpawnTimout = 5000;
+    this.oxygenSpawnTimout = 10000;
     this.dead = false;
     this.score = 0;
     this.isScoreBlinking = false;
@@ -58,7 +62,6 @@ class GameEngine {
     this.spaceship.draw();
 
     for (const gameEntity of this.gameEntities) {
-      debugger;
       gameEntity.draw();
     }
     this.displayScore();
@@ -126,7 +129,6 @@ class GameEngine {
   private checkHitEnemy(entity: GameEntity, index: number) {
     if (!this.spaceship.laserBeams) return;
     if (entity instanceof OxygenTank) return;
-    // const clonedLaserBeam = [...this.spaceship.laserBeams];
 
     for (let i = 0; i < this.spaceship.laserBeams.length; i++) {
       const laserBullet = this.spaceship.laserBeams[i];
@@ -136,44 +138,61 @@ class GameEngine {
         laserBullet.position.y < entity.position.y + entity.size.y &&
         laserBullet.size.y + laserBullet.position.y > entity.position.y
       ) {
+        if (!(entity.hp <= 0)) {
+          entity.hp -= 1
+          this.spaceship.laserBeams.splice(i, 1);
+
+          if (entity instanceof Astroid) {
+            entity.image = asteroidHit;
+            setTimeout (() => {
+              entity.image = asteroid
+            }, 150);
+          }
+
+          if (entity instanceof Alien) {
+            entity.image = alienHit;
+            setTimeout (() => {
+              entity.image = alien
+            }, 150);
+          }
+          return;
+        }
         this.clonedGameEntitiy.splice(index, 1);
         this.spaceship.laserBeams.splice(i, 1);
-        return;
       }
     }
-    // this.spaceship.laserBeams = clonedLaserBeam;
   }
 
   private spawnAsteroid() {
-    this.spawnTimout -= deltaTime;
-    if (this.spawnTimout < 0) {
+    this.asteroidSpawnTimout -= deltaTime;
+    if (this.asteroidSpawnTimout < 0) {
       const x = random(0, width);
-      const y = random(-20, -height);
+      const y = random(-100, -height);
       const position = createVector(x, y);
       this.gameEntities.push(new Astroid(position));
-      this.spawnTimout = random(3000, 8000);
+      this.asteroidSpawnTimout = random(1000, 2000);
     }
   }
 
   private spawnAlien() {
-    this.spawnTimout -= deltaTime;
-    if (this.spawnTimout < 0) {
+    this.alienSpawnTimout -= deltaTime;
+    if (this.alienSpawnTimout < 0) {
       const x = random(0, width);
-      const y = random(-20, -height);
+      const y = random(-100, -height);
       const position = createVector(x, y);
       this.gameEntities.push(new Alien(position));
-      this.spawnTimout = random(1000, 5000);
+      this.alienSpawnTimout = random(1000, 5000);
     }
   }
 
   private spawnOxygenTank() {
-    this.spawnTimout -= deltaTime;
-    if (this.spawnTimout < 0) {
+    this.oxygenSpawnTimout -= deltaTime;
+    if (this.oxygenSpawnTimout < 0) {
       const x = random(0, width);
-      const y = random(0, -height);
+      const y = random(-100, -height);
       const position = createVector(x, y);
       this.gameEntities.push(new OxygenTank(position));
-      this.spawnTimout = random(1000, 8000);
+      this.oxygenSpawnTimout = random(1000, 20000);
     }
   }
 }
