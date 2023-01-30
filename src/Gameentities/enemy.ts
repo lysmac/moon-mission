0862edd21033c386947a151085e00b68a79f4402
ye-mou <git.ymo22@gmail.com>
2023-01-30 22:13:09 +0100

    changed hp in enemy to public. Minor change dosent affect game

--- src/Gameentities/enemy.ts
+++ src/Gameentities/enemy.ts
@@ -2,10 +2,10 @@
 
 class Enemy extends GameEntity {
   protected isAlive: boolean;
-  protected hp: number;
+  public hp: number;
 
   constructor(position: p5.Vector, size: p5.Vector, image: p5.Image, hp: number) {
-    super(position, size, image)
+    super(position, size, image, hp)
     
     this.isAlive = true;
     this.hp = hp;
