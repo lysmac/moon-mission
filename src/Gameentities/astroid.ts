909b3a5e22b903ece73b830345608ceb516b3dec
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-31 18:35:37 +0100

    set hp value

--- src/Gameentities/astroid.ts
+++ src/Gameentities/astroid.ts
@@ -4,7 +4,7 @@ class Astroid extends GameEntity {
 
   constructor(position: p5.Vector) {
     const size = createVector(125, 125);
-    const hp = 1;
+    const hp = 2;
     super(position, size, asteroid, hp); 
   }
 }
