9718023f0726509686174360b3c5ba207debe4ca
ParhamInBinart <parham.berenjian@gmail.com>
2023-01-31 18:36:05 +0100

    Added Score as entity

--- src/Gameentities/score.ts
+++ src/Gameentities/score.ts
@@ -2,11 +2,26 @@
 // till scoreBoard? ScoreBoard är ihop med Game.
 
 class Score extends GameEntity {
-  constructor() {
-    super()
+  private speed: number;
+  private score: string;
+  private color: p5.Color;
+
+  constructor(position: p5.Vector, score: string) {
+
+    super(position, 0)
+
+    this.speed = 1;
+    this.score = score;
+    this.color = color(255, 255, 0);
   }
 
-  public update() {}
+  public update() {
+    this.position.y -= this.speed;
+  }
 
-  public draw() {}
+  public draw() {
+    textSize(24);
+    text(this.score, this.position.x, this.position.y);
+    fill(this.color)
+  }
 }
