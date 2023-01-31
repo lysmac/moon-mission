f8ab42768fdac61548f1850135e28f90b29ede58
MadeleineGustafson <madeleinegu@gmail.com>
2023-01-31 14:18:42 +0100

    Design fix on scoreboard

--- src/scoreboard.ts
+++ src/scoreboard.ts
@@ -39,21 +39,22 @@ class ScoreBoard {
     textSize(70);
     textAlign(CENTER);
 
-    textFont("sofia sans");
-    text(
-      "MOON MISSION",
-      this.position.x + this.size.x / 2,
-      this.position.y - 90
-    );
+    // textFont("sofia sans");
+    // text(
+    //   "MOON MISSION",
+    //   this.position.x + this.size.x / 2,
+    //   this.position.y - 90
+    // );
+    image(header, 0, 0,);
 
     // SCOREBOARD - SUBTITLE
     textSize(55);
     textAlign(CENTER);
+  
     text("SCOREBOARD", this.position.x + this.size.x / 2, this.position.y - 10);
 
     // SCORE LIST
     let highscores = this.getTopFiveScores();
-
     fill("#D9D9D9");
     textSize(30);
     if (highscores.length === 0) {
@@ -63,14 +64,14 @@ class ScoreBoard {
       text(
         i + 1 + ". " + highscores[i],
         this.position.x + 200,
-        this.position.y + 80 + i * 40
+        this.position.y + 50 + i * 40
       );
     }
     textSize(25);
     text(
       "PRESS M TO RETURN TO MENU",
       this.position.x + this.size.x / 2,
-      this.position.y + 280
+      this.position.y + 270
     );
   }
 
