8b9f24781e3a75362330fd4296fddc197cce737a
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-02 00:42:34 +0100

    added template for hitbox shapes

--- src/Gameentities/alien.ts
+++ src/Gameentities/alien.ts
@@ -10,11 +10,28 @@ class Alien extends GameEntity {
     this.velocity = createVector(0.5, 1);
   }
 
-  update() {
+  public update() {
     this.position.x += this.velocity.x * deltaTime;
     if (this.position.x < -width/6 || this.position.x > width) {
       this.velocity.x = -this.velocity.x;
     }
     super.update();
   }
+
+  // public draw() {
+    // image(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
+
+    // alien hitbox shape
+    // push();
+    // fill(255);
+    // triangle(this.position.x + 2, this.position.y + 55, 
+    //   this.position.x + this.size.x / 2, this.position.y - 2, 
+    //   this.position.x + this.size.x - 2, this.position.y + 55);
+
+    // ellipse(this.position.x + this.size.x / 2, 
+    // this.position.y + 65, 
+    // this.size.x, 
+    // this.size.y - 35);
+    // pop();
+  // }
 }
